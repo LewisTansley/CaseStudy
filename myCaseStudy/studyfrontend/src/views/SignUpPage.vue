1466486fadb30ede509d9460604b0750a73b46cf
Lewis Tansley <officialinsanityproject@gmail.com>
2023-08-02 21:04:24 +0100

    Create SignUpPage.vue

---
+++ myCaseStudy/studyfrontend/src/views/SignUpPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <nav><router-link></router-link></nav>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <LoginPrompt/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginPrompt from '@/components/LoginPrompt.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    LoginPrompt
+  }
+}
+</script>
