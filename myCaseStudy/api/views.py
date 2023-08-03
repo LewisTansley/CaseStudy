090016dc52e034313f5ec152fac2502600b0a39e
Lewis Tansley <officialinsanityproject@gmail.com>
2023-08-03 19:03:08 +0100

    latest

--- myCaseStudy/api/views.py
+++ myCaseStudy/api/views.py
@@ -8,11 +8,12 @@ Research sqlalchemy for constructing databse schema
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import viewsets
-from myCaseStudy.models import Book
-from myCaseStudy.serializers import BookSerializer
+from rest_framework import serializers
+from myCaseStudy.models import Book, User
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -24,6 +25,20 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+class BookSerializer(serializers.ModelSerializer):
+    bookTitle = serializers.CharField(max_length=180)
+    bookAuthor = serializers.CharField(max_length=180)
+    bookDescription = serializers.CharField(max_length=180)
+    bookCover = serializers.ImageField()
+    class Meta:
+        model = Book
+        fields = "__all__"
 
 class BookViewSet(viewsets.ModelViewSet):
     """
